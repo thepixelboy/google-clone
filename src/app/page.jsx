39c19dbc9a1efeedb8050bdafde731a4c9b561e1
Ruben <34570952+thepixelboy@users.noreply.github.com>
2023-04-08 19:23:55 +0200

    feat: add gome header component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,9 @@
+import HomeHeader from "@/components/HomeHeader";
+
 export default function Home() {
-  return <h1 className="text-6xl">Home</h1>;
+  return (
+    <>
+      <HomeHeader />
+    </>
+  );
 }
