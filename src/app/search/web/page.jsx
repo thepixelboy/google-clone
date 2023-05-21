5e4f51b0288622a680d9fda9fe836d26ed7f81d0
Ruben <34570952+thepixelboy@users.noreply.github.com>
2023-05-21 17:33:46 +0200

    feat: fetch data from google search api and show titles

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,3 +1,14 @@
-export default function WebSearchPage() {
-  return <div>WebSearchPage</div>;
+export default async function WebSearchPage({ searchParams }) {
+  const response =
+    await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_JSON_API_KEY}&cx=${process.env.GOOGLE_JSON_CONTEXT_KEY}&q=${searchParams.term}
+`);
+
+  const data = await response.json();
+  const results = data.items;
+  return (
+    <>
+      {results &&
+        results.map((result) => <h1 key={result.title}>{result.title}</h1>)}
+    </>
+  );
 }
