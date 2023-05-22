b9a57d34359c47a1bfd1faca07ec81444c4bd251
Ruben <34570952+thepixelboy@users.noreply.github.com>
2023-05-22 18:27:46 +0200

    feat: handle possible errors

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,10 +1,32 @@
+import Link from "next/link";
+
 export default async function WebSearchPage({ searchParams }) {
   const response =
     await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_JSON_API_KEY}&cx=${process.env.GOOGLE_JSON_CONTEXT_KEY}&q=${searchParams.term}
 `);
 
+  if (!response.ok) {
+    throw new Error("Something went wrong");
+  }
+
   const data = await response.json();
   const results = data.items;
+
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">No results found</h1>
+        <p className="text-lg">
+          Try searching for something else or{" "}
+          <Link className="text-blue-500" href="/">
+            go back to the homepage
+          </Link>
+          .
+        </p>
+      </div>
+    );
+  }
+
   return (
     <>
       {results &&
